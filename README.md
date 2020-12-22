54821a1845eebd01f89ba7192a3426b9da4a7fa0
Lexxus <darklexxus@outlook.com>
2020-12-21 23:55:29 -0600

    Finish styles -> Done

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ## Part of [The Odin Project](https://www.theodinproject.com/) curriculum.
 
 ### You can see it live [here](https://darklania.github.io/calculator/).
+
+### Keyboard usage:
+
+Numbers and Numpad numbers -> 0 1 2 3 4 5 6 7 8 9
+Period and Numpad decimal -> .
+Slash -> +/-
+Escape -> CLEAR
+Del and backspace -> DELETE
+Numpad Operators -> ÷ x - +
--- script.js
+++ script.js
@@ -23,73 +23,149 @@ function manageInput(e){
         case 'Digit0':
         case 'Numpad0':
             document.getElementById('zero').click();
+            document.getElementById('zero').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('zero').classList.remove('press');
+               },50);
             break;
         case 'Digit1':
         case 'Numpad1':
             document.getElementById('one').click();
+            document.getElementById('one').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('one').classList.remove('press');
+               },50);
             break;
         case 'Digit2':
         case 'Numpad2':
             document.getElementById('two').click();
+            document.getElementById('two').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('two').classList.remove('press');
+               },50);
             break;
         case 'Digit3':
         case 'Numpad3':
             document.getElementById('three').click();
+            document.getElementById('three').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('three').classList.remove('press');
+               },50);
             break;
         case 'Digit4':
         case 'Numpad4':
             document.getElementById('four').click();
+            document.getElementById('four').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('four').classList.remove('press');
+            },50);
             break;
         case 'Digit5':
         case 'Numpad5':
             document.getElementById('five').click();
+            document.getElementById('five').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('five').classList.remove('press');
+            },50);
             break;
         case 'Digit6':
         case 'Numpad6':
             document.getElementById('six').click();
+            document.getElementById('six').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('six').classList.remove('press');
+            },50);
             break;
         case 'Digit7':
         case 'Numpad7':
             document.getElementById('seven').click();
+            document.getElementById('seven').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('seven').classList.remove('press');
+            },50);
             break;
         case 'Digit8':
         case 'Numpad8':
             document.getElementById('eight').click();
+            document.getElementById('eight').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('eight').classList.remove('press');
+            },50);
             break;
         case 'Digit9':
         case 'Numpad9':
             document.getElementById('nine').click();
+            document.getElementById('nine').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('nine').classList.remove('press');
+            },50);
             break;
         case 'Period':
         case 'NumpadDecimal':
             document.getElementById('dot').click();
+            document.getElementById('dot').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('dot').classList.remove('press');
+            },50);
             break;
         case 'Slash':
             document.getElementById('sign').click();
+            document.getElementById('sign').classList.add('press');
+            setTimeout(()=>{
+                document.getElementById('sign').classList.remove('press');
+            },50);
             break;
         case 'Backspace':
         case 'Delete':
             document.getElementById('del').click();
+            document.getElementById('del').classList.add('delet');
+            setTimeout(()=>{
+                document.getElementById('del').classList.remove('delet');
+            },50);
             break;
         case 'Escape':
             document.getElementById('clear').click();
+            document.getElementById('clear').classList.add('pressCeq');
+            setTimeout(()=>{
+                document.getElementById('clear').classList.remove('pressCeq');
+               },50);
             break;
         case 'NumpadDivide':
             e.preventDefault();
             document.getElementById('div').click();
+            document.getElementById('div').classList.add('opi');
+            setTimeout(()=>{
+                document.getElementById('div').classList.remove('opi');
+            },50);
             break;
         case 'NumpadMultiply':
             document.getElementById('mul').click();
+            document.getElementById('mul').classList.add('opi');
+            setTimeout(()=>{
+                document.getElementById('mul').classList.remove('opi');
+            },50);
             break;
         case 'NumpadSubtract':
             document.getElementById('minus').click();
+            document.getElementById('minus').classList.add('opi');
+            setTimeout(()=>{
+                document.getElementById('minus').classList.remove('opi');
+            },50);
             break;
         case 'NumpadAdd':
             document.getElementById('plus').click();
+            document.getElementById('plus').classList.add('opi');
+            setTimeout(()=>{
+                document.getElementById('plus').classList.remove('opi');
+            },50);
             break;
         case 'Enter':
         case 'NumpadEnter':
             document.getElementById('eq').click();
+            document.getElementById('eq').classList.add('pressCeq');
+            setTimeout(()=>{
+                document.getElementById('eq').classList.remove('pressCeq');
+               },50);
             break;
         default:
             break;
